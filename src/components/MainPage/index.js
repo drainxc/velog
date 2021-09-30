353ea81bee3679b2e602bec45768ba5c716192a6
ldh7228 <ldh7228@gmail.com>
2021-09-30 22:43:42 +0900

    [ADD] MainPage

---
+++ src/components/MainPage/index.js
@@ -0,0 +1,21 @@
+import Post from "../Post";
+import * as S from "./styles";
+import { data } from "../../lib/export/data";
+
+export default function Mainpage() {
+    console.log(data)
+  return (
+    <S.MainDiv>
+      <div className="container">
+        {data.map((post) => (
+          <Post
+            title={post.title}
+            writer={post.name}
+            backgroundImage={post.cover_url}
+            contents={post.descriptionx}
+          />
+        ))}
+      </div>
+    </S.MainDiv>
+  );
+}
