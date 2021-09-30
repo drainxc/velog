075af097a78b381cd8d4458199c12231d971d886
ldh7228 <ldh7228@gmail.com>
2021-09-30 22:44:29 +0900

    [DESIGN] Mainpage
    메인 페이지 디자인 추가

---
+++ src/components/MainPage/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+    margin: 30px 100px;
+  .container {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-row-gap: 20px;
+  }
+`;
