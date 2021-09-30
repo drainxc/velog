ec3be78312c120e9774dc097e9d70a7e6ccc058f
ldh7228 <ldh7228@gmail.com>
2021-09-30 22:41:41 +0900

    [DESIGN] post

---
+++ src/components/Post/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const PostDiv = styled.div`
+  .post {
+    width: 275px;
+    height: 365px;
+    img {
+      width: 275px;
+      height: 175px;
+      object-fit: cover;
+    }
+    .title {
+      font-size: 25px;
+      font-weight: bold;
+    }
+    .contents {
+      height: 100px;
+    }
+  }
+`;
