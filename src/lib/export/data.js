f89af4108d5edfb713c3bc3b0f52f4325a9ba52b
ldh7228 <ldh7228@gmail.com>
2021-09-30 22:46:31 +0900

    [ADD] data
    더미데이터 추가

---
+++ src/lib/export/data.js
@@ -0,0 +1,45 @@
+export const data = [
+  {
+    title: "프로필 꾸미기",
+    name: "ㅁㄴㅇㄹ",
+    cover_url:
+      "https://media.vlpt.us/images/dding_ji/post/c86b7373-6028-44a5-9a10-db838a114280/20210922_154202.png?w=640",
+    description:
+      "[Github] Readme 나만의 프로필(Profile) 꾸미기 - 아이콘(인스타, 벨로그), 스탯, 랭크(solved.ac)",
+    id: 1,
+  },
+  {
+    title: "CORS를 처음 마주하는 분들에게",
+    name: "ㅂㅈㄷㄱ",
+    cover_url:
+      "https://media.vlpt.us/images/young_pallete/post/374c04f3-dcfe-4f15-a266-789e77a5e644/image.png?w=640",
+    description: "CORS에 대해 내가 알고 있는 것들",
+    id: 2,
+  },
+  {
+    title: "프론트엔드 면접 질문",
+    name: "adsf",
+    cover_url:
+      "https://media.vlpt.us/images/koremp/post/41cec128-5eca-4bba-9de8-74852fe1b224/ar.jpg?w=640",
+    description:
+      "회사 20개 면접 광탈 이후 지금까지의 과정, 60초 후에 공개됩니다!",
+    id: 3,
+  },
+  {
+    title: "개인적인 Flutter 꿀팁 모음",
+    name: "qwrety",
+    cover_url:
+      "https://media.vlpt.us/images/sangh518/post/4ad441c3-a75f-4954-ac67-85888ecb4c43/%E1%84%8C%E1%85%A6%E1%84%86%E1%85%A9%E1%86%A8%E1%84%8B%E1%85%B3%E1%86%AF%20%E1%84%8B%E1%85%B5%E1%86%B8%E1%84%85%E1%85%A7%E1%86%A8%E1%84%92%E1%85%A2%E1%84%8C%E1%85%AE%E1%84%89%E1%85%A6%E1%84%8B%E1%85%AD_-001.png?w=640",
+    description:
+      "본 글은 플러터를 사용하면서 개인적으로 유용했던 사항들을 기록한 글입니다",
+    id: 4,
+  },
+  {
+    title: "우리는 이미 '어댑터 패턴'을 알고 있다",
+    name: "1010",
+    cover_url:
+      "https://media.vlpt.us/images/haero_kim/post/84adee42-3e2f-4b9f-8ae5-d40d66e6d395/adapter.png?w=640",
+    description: "실생활로부터 Adapter Pattern 이해하기",
+    id: 5,
+  },
+];
