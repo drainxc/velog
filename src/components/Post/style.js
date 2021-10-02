95362c4b3f5bef9bb209b84ff28f70df9fbcdcb9
ldh7228 <ldh7228@gmail.com>
2021-10-02 14:02:26 +0900

    [DESIGN] Post
    Post.js 디자인 전체적으로 수정

--- src/components/Post/style.js
+++ src/components/Post/style.js
@@ -2,46 +2,51 @@ import styled from "styled-components";
 
 export const PostDiv = styled.div`
   .main {
+    position: relative;
     overflow-x: hidden;
     overflow-y: hidden;
     border-radius: 12px;
-    width: 275px;
+    width: 300px;
     height: 400px;
+    background-color: #ffffff;
   }
   img {
-    width: 275px;
+    width: 300px;
     height: 175px;
     object-fit: cover;
   }
   .title {
     font-size: 25px;
     font-weight: bold;
-    width: 275px;
+    width: 300px;
     margin-left: 5px;
     text-decoration-line: none;
   }
   .contents {
     font-size: 15px;
-    width: 275px;
+    width: 300px;
     margin-left: 5px;
   }
   .post {
     height: 165px;
   }
   hr {
-    width: 275px;
+    width: 300px;
+    background-color: #000000;
   }
   .writer {
     margin-left: 5px;
   }
   .increase {
+    position: relative;
     overflow-x: hidden;
     overflow-y: hidden;
     border-radius: 12px;
-    width: 275px;
+    width: 300px;
     height: 400px;
+    background-color: #ffffff;
     transition: all 0.1s ease;
     transform: scale(1.1);
-    box-shadow: 0px 0px 10px grey;
+    box-shadow: 0px 0px 20px rgb(180, 180, 180);
   }
 `;
