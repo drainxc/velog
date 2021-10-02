a7b6a01c86742b81d630cab05ffdbb4ad4397420
ldh7228 <ldh7228@gmail.com>
2021-10-02 13:58:39 +0900

    [DESIGN] Container
    index.js 디자인 추가

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,21 +2,24 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "../Header";
 import Mainpage from "../MainPage";
 import PostDetail from "../PostDetail";
+import * as S from "./styles";
 
-export default function container() {
+export default function Container() {
   return (
     <>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Header />
-            <Mainpage />
-          </Route>
-          <Route path="/detail/:postID">
-              <PostDetail/>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <S.ContainerDiv>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Header />
+              <Mainpage />
+            </Route>
+            <Route path="/detail/:postID">
+              <PostDetail />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </S.ContainerDiv>
     </>
   );
 }
---
+++ src/components/Container/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ContainerDiv = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: auto;
+  background-color: #f8f9fa;
+`;
