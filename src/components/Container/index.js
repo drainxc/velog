ada99d12ce5d161ab478e130752e0f4b93e1cba9
ldh7228 <ldh7228@gmail.com>
2021-10-02 14:12:22 +0900

    [DESIGN] Container
    Container 배경색 추가

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,25 +1,27 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "../Header";
-import Mainpage from "../MainPage";
+import MainPage from "../MainPage";
 import PostDetail from "../PostDetail";
 import * as S from "./styles";
 
 export default function Container() {
   return (
     <>
-      <S.ContainerDiv>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/">
-              <Header />
-              <Mainpage />
-            </Route>
-            <Route path="/detail/:postID">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <S.MainPageDiv>
+              <Header classname="Header" />
+              <MainPage classname="MainPage" />
+            </S.MainPageDiv>
+          </Route>
+          <Route path="/detail/:postID">
+            <S.PostDetailDiv>
               <PostDetail />
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </S.ContainerDiv>
+            </S.PostDetailDiv>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 
-export const ContainerDiv = styled.div`
+export const MainPageDiv = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: auto;
   background-color: #f8f9fa;
+`;
+
+export const PostDetailDiv = styled.div`
+  background-color: "#ffffff";
 `;
