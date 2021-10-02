ea405d6766679ced8ab081b429559a8b38059cbd
ldh7228 <ldh7228@gmail.com>
2021-10-02 11:30:24 +0900

    [ADD] increase animation
    post.js 게시물 크기 증가 애니메이션

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import * as S from "./style";
 
 export default function Post({ title, writer, backgroundImage, contents }) {
-    return (
-    <S.PostDiv>
-      <img src={backgroundImage} alt="" />
-      <div className="post">
-        <div className="title">{title}</div>
-        <div className="contents">{contents}</div>
+  const [onMouse, SetOnMouse] = useState(false);
+
+  function onMouseOverEvent() {
+    SetOnMouse(true);
+  }
+
+  function onMouseLeaveEvent() {
+    SetOnMouse(false);
+  }
+
+  return (
+    <S.PostDiv onMouseOver={onMouseOverEvent} onMouseLeave={onMouseLeaveEvent}>
+      <div className={onMouse ? "increase" : "main"}>
+        <img src={backgroundImage} alt="" />
+        <div className="post">
+          <div className="title">{title}</div>
+          <div className="contents">{contents}</div>
+        </div>
+        <hr />
+        <div className="writer">{writer}</div>
       </div>
-      <hr width="300px" />
-      <div className="writer">{writer}</div>
     </S.PostDiv>
   );
 }
--- src/components/Post/style.js
+++ src/components/Post/style.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const PostDiv = styled.div`
-  overflow-x: hidden;
-  overflow-y: hidden;
-  border-radius: 12px;
-  width: 275px;
-  height: 400px;
+  .main {
+    overflow-x: hidden;
+    overflow-y: hidden;
+    border-radius: 12px;
+    width: 275px;
+    height: 400px;
+  }
   img {
     width: 275px;
     height: 175px;
@@ -14,11 +16,26 @@ export const PostDiv = styled.div`
   .title {
     font-size: 25px;
     font-weight: bold;
+    width: 275px;
   }
   .contents {
     font-size: 15px;
+    width: 275px;
   }
   .post {
     height: 165px;
+  }
+  hr {
+    width: 275px;
+  }
+  .increase {
+    overflow-x: hidden;
+    overflow-y: hidden;
+    border-radius: 12px;
+    width: 275px;
+    height: 400px;
+    transition: all 0.1s ease;
+    transform: scale(1.1);
+    box-shadow: 0px 0px 10px grey;
   }
 `;
