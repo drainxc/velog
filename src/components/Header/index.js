82f36a7fad43acf4baa6202861a1049a34bb25bd
ldh7228 <ldh7228@gmail.com>
2021-10-03 15:37:48 +0900

    [ADD] move event
    line 애니메이션 추가

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import * as S from "./styles";
 import logo from "../../asset/velog.png";
 import search from "../../asset/search.png";
 import arrow from "../../asset/stock.png";
 import clock from "../../asset/clock.png";
+import gsap from "gsap";
 
 export default function Header() {
+
+  const el = useRef();
+  const q = gsap.utils.selector(el);
+  
+  function leftMove() {
+    gsap.to(q(".line"), { x: 0 });
+  }
+
+  function rightMove() {
+    gsap.to(q(".line"), { x: 110 });
+  }
+
   return (
-    <S.HeaderDiv>
+    <S.HeaderDiv ref={el}>
       <div className="top">
         <span>
           <img src={logo} alt="" className="logo" />
@@ -18,16 +31,16 @@ export default function Header() {
         </span>
       </div>
       <div className="bottom">
-        <button>
+        <button onClick={leftMove}>
           <img src={arrow} alt="" className="tranding" />
           <span>트렌딩</span>
         </button>
-        <button>
+        <button onClick={rightMove}>
           <img src={clock} ait="" className="latest" />
           <span>최신</span>
         </button>
       </div>
-      <hr width="100px"/>
+        <hr width="100px" className="line" />
     </S.HeaderDiv>
   );
 }
