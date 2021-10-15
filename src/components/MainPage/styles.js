664dc2b7289e27c024de8a594851a75142abe77f
ldh7228 <ldh7228@gmail.com>
2021-10-15 11:26:22 +0900

    [DESIGN]

--- src/components/MainPage/styles.js
+++ src/components/MainPage/styles.js
@@ -3,12 +3,11 @@ import styled from "styled-components";
 export const MainDiv = styled.div`
   /*   margin: 30px 100px; */
   .container {
-    width: 1500px;
-    overflow: hidden;
+    width: 1250px;
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(auto-fill, minmax(20%, auto));
     grid-row-gap: 30px;
     grid-column-gap: 20px;
-    padding-bottom: 30px;
+    padding: 30px 9%;
   }
 `;
