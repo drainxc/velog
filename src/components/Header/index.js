984a00caa2a277d5289b562123b63ea102383717
ldh7228 <ldh7228@gmail.com>
2021-10-15 11:57:36 +0900

    [ADD] Link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,7 @@ import search from "../../asset/search.png";
 import arrow from "../../asset/stock.png";
 import clock from "../../asset/clock.png";
 import gsap from "gsap";
+import { Link } from "react-router-dom";
 
 export default function Header({ change, title }) {
   const el = useRef();
@@ -17,10 +18,12 @@ export default function Header({ change, title }) {
   return (
     <S.HeaderDiv ref={el}>
       <div className="top">
-        <span className="mainTitle">
-          {change ? "" : <img src={logo} alt="" />}
-          <span>{title}</span>
-        </span>
+        <Link to="/" style={{ textDecoration: "none", color: "#000000" }}>
+          <span className="mainTitle">
+            {change ? "" : <img src={logo} alt="" />}
+            <span>{title}</span>
+          </span>
+        </Link>
         <span>
           <span className="search">
             <img src={search} alt="" />
